7c9e8bca8f3cd585b20e17fa591c3b3806cb2f30
Adler Luders <adlerluders@gmail.com>
2020-04-27 12:38:26 -0400

    Final updates made prior to submit;

--- app.js
+++ app.js
@@ -4,9 +4,3 @@ mainDisplay = document.getElementById("main");
 toggler.addEventListener("change", () => {
   mainDisplay.classList.toggle("is-dark-mode");
 });
-
-/**
- * Grabbed Toggler
- * Grabbed overall display wrapper which was the main tag
- * add 'change' event with an anonymous function which toggles a class name defined in dark-mode.css
- */
